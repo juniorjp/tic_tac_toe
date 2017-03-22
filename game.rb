d93279c2fc578f378792000ae4ec91aff1557656
Reginaldo Junior <juniorjp1989@gmail.com>
2017-03-22 11:43:52 -0300

    add colorize

--- game.rb
+++ game.rb
@@ -1,5 +1,6 @@
 require_relative 'services/input_service'
 require_relative 'helper/string_helper'
+require 'colorize'
 
 class Game
   def initialize
@@ -97,7 +98,7 @@ class Game
   private
 
   def print_board
-    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} \n===+===+===\n #{@board[3]} | #{@board[4]} | #{@board[5]} \n===+===+===\n #{@board[6]} | #{@board[7]} | #{@board[8]} \n"
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} \n===+===+===\n #{@board[3]} | #{@board[4]} | #{@board[5]} \n===+===+===\n #{@board[6]} | #{@board[7]} | #{@board[8]} \n".colorize(:light_blue)
   end
 
 end
